361fda459584e45bff1dee36c0c4f4156abc69ec
renpoo <renpoo@gmail.com>
2026-03-28 11:32:48 +0900

    Trying to Generate Dummy Journal Data (Rev.2)

--- bin/orchestrators/1_1_run_fk_simulation.sh
+++ bin/orchestrators/1_1_run_fk_simulation.sh
@@ -4,4 +4,4 @@ source "$(dirname "$0")/_tlu_env.sh"
 run_tlu_pipeline "Forward Kinematics (FK) Filter" \
     "Src" "Tgt" \
     "src.filters._1_1_filter_fk_simulation" "result.1_1_filter_fk.analysis.csv" \
-    --fk_input_mode="static" --static_dq_labels="ACC_Sales_Revenue_Global:4400" --gamma=0.85 --max_k=5 --node_map="${TLU_NODE_MAP}"
+    --fk_input_mode="static" --static_dq_labels="ACC_Sales_Revenue:4400" --gamma=0.85 --max_k=5 --node_map="${TLU_NODE_MAP}"
