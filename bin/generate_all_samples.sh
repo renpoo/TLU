2c3bf893f7908b795b6c377ae92fcfcd37ad7cd8
renpoo <renpoo@gmail.com>
2026-04-27 10:19:58 +0900

    Delete generate_all_samples.sh

--- bin/generate_all_samples.sh
+++
@@ -1,7 +0,0 @@
-generate_sample 4_Composite_Chaos 0.005 0.01 0.005 0.005
-
-# 5_Kyoto_Traffic
-echo "==================================="
-echo "Generating Sample 5_Kyoto_Traffic..."
-export TARGET_ENV="samples/Sample_5_Kyoto_Traffic"
-# We need to run batch_generate_dummy_traffic_data.sh but modify it first to support TARGET_ENV
